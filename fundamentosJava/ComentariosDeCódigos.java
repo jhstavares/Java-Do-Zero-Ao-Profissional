eaf234e0a9e7ccbdf6d6e4262b10ff3c349d2e47
jhtavares_01@htmail.com <jhstavares_01@hotmail.com>
2023-02-20 17:34:30 -0300

    feat: Add nova pasta com o conteúdo.

---
+++ fundamentosJava/ComentariosDeCódigos.java
@@ -0,0 +1,12 @@
+package fundamentosJava;
+
+public class ComentariosDeCódigos {
+    // Comentarios de uma linha é feita com barra barra antes da declaração
+
+    /* Comentários de multiplas linhas é feito */
+
+    /**
+     * É comentario de java doc
+     *
+     */
+}
