65476ad4e18941149a887b5cb6fe122e6b1e2e47
jhtavares_01@htmail.com <jhstavares_01@hotmail.com>
2023-02-20 15:33:33 -0300

    featu: Visão de Algoritimo e EstrutaDeDados

---
+++ introducao/Algoritimo.java
@@ -0,0 +1,8 @@
+package introducao;
+
+public class Algoritimo {
+
+    // Algorimo é uma sequência de passos a serem seguido,
+    //para chegar a um objetivo
+
+}
---
+++ introducao/EstrutaDeDados.java
@@ -0,0 +1,19 @@
+package introducao;
+
+public class EstrutaDeDados {
+    // Estrura visa organizar e administrar e ordem dos dados.
+
+    // FIFO - O primeiro que chega é o primeiro que sai.
+
+    // PILHA/LIFO - O último a entrar é o primeiro a sair.
+
+    // ARVORES - Um arquivo dentro de outro
+
+    // Estrutura de dados básicas: INT - REAL - LETRA - BOOL
+
+    // Variáveis e constantes - Onde os dados são armazenados.
+
+    // Operações: Atrib. Arit. Relac. Lógic.
+
+    // Estruturas personalizadas
+}
